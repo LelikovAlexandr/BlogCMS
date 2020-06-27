1e61b5779cf6082f86cae9cdbdd801a15baadf74
AlexandrLelikov <houda.lamsaaf@ump.ac.ma>
2020-06-27 18:30:16 +0300

    Added price management

--- users/views.py
+++ users/views.py
@@ -165,4 +165,8 @@ class DeleteUser(DeleteView, LoginRequiredMixin):
 
 class RenewSubscription(TemplateView):
     template_name = 'users/renew_subscription.html'
-    extra_context = {'prices': Price.objects.all()}
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['prices'] = Price.objects.all()
+        return context
